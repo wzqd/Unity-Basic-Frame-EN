8f62ed3efdb751a40465469bdf02691276b6d4f6
Jason Gu <1057337859@qq.com>
2022-03-23 12:48:44 -0700

    Comments for singleton done

---
+++ Assets/scripts/BasicFrame/Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region Singleton Base Class
+//To simplify the usage of singleton pattern
+//Classes who inherit this singleton base class will be a singleton class
+//Use   classname.instance.method()   to call the methods in singleton classes
+//----methods MonoBehavior will be unavailable after inheritance----
+//----this singleton mode uses lazy loading, ignoring multiply threading conditions----
+#endregion
+
+public class Singleton<T> where T:new()
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+        if (instance == null)
+                instance = new T();
+        return instance;
+        }
+    }
+}
+
---
+++ Assets/scripts/BasicFrame/Singleton/SingletonMono.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region Singleton Base Class Inheriting MonoBehavior
+//Classes who inherit this class will be singleton class, and also have access to methods in MonoBehavior
+//----this singleton mode is lazy loading, ignoring multiply threading conditions----
+#endregion
+
+public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if( instance == null )
+            {
+                GameObject obj = new GameObject {name = typeof(T) + "_Singleton"};
+                instance = obj.AddComponent<T>();
+                DontDestroyOnLoad(obj);
+            } 
+            return instance;
+        }
+    }
+}
