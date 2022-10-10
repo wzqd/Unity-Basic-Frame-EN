1797b9cddfe670cbe201de19c261bcb6b83fe437
Jason Gu <1057337859@qq.com>
2022-10-10 09:07:34 -0700

    support text mesh pro

--- Assets/scripts/BasicFrame/UI/BasePanel.cs
+++ Assets/scripts/BasicFrame/UI/BasePanel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -39,6 +40,7 @@ public class BasePanel : MonoBehaviour
         FindChildrenComponent<Image>();
         FindChildrenComponent<ScrollRect>();
         FindChildrenComponent<InputField>();
+        FindChildrenComponent<TextMeshProUGUI>();
     }
 
     /// <summary>
